d1ad1c478a22bd72a8f22b83c0251dd7b99a6d64
antekulis <ante.kulis@optiva.com>
2025-10-31 05:53:39 +0100

    Add start_server module and HTTP handlers skeleton

---
+++ src/start_server.erl
@@ -0,0 +1,14 @@
+-module(start_server).
+-export([start/0]).
+
+start() ->
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/jobs", jobs_handler, []},
+            {"/jobs/script", jobs_script_handler, []}
+        ]}
+    ]),
+    {ok, _} = cowboy:start_clear(http_listener, [{port, 8080}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+    io:format("Server running on http://localhost:8080/~n").
