a5ccf089f116adfd0c425548a4ca2b08ab92affa
antekulis <ante.kulis@optiva.com>
2025-11-03 18:37:38 +0100

    Add jobs_script_handler for script representation

--- src/jobs_script_handler.erl
+++ src/jobs_script_handler.erl
@@ -1,1 +1,20 @@
 -module(jobs_script_handler).
+-behaviour(cowboy_handler).
+-export([init/2]).
+
+init(Req, State) ->
+    {ok, Body, Req1} = cowboy_req:read_body(Req),
+    Json = jiffy:decode(Body, [return_maps]),
+    Tasks = maps:get(<<"tasks">>, Json, []),
+    case job_sort:sort_tasks(Tasks) of
+        {ok, SortedTasks} ->
+            Commands = [maps:get(<<"command">>, T) || T <- SortedTasks],
+            Joined = binary:join(Commands, <<"\n">>),
+            BashScript = <<"#!/usr/bin/env bash\n", Joined/binary, "\n">>,
+            Reply = cowboy_req:reply(200,
+                #{<<"content-type">> => <<"text/plain">>},
+                BashScript, Req1),
+            {ok, Reply, State};
+        {_Error, _Reason} ->
+            {ok, _Error, State}
+    end.
